2707fb74d0e22e3bafb30589266182291f3b5ed6
shrutika158 <145742140+shrutika158@users.noreply.github.com>
2024-07-27 11:44:53 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router , Routes, } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/jquery/dist/jquery';
+import Header from './components/header';
+
+// import { Route} from "react-router";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+
+
+
+
+
+
+
+
+
+
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
